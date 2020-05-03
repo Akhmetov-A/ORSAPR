a95221cee16f6255af36ef7efa8d29ba1da311db
AkhmetovAlen <Alen-007@mail.ru>
2020-05-03 21:25:57 +0700

    Разработан макет программы

--- GlassDesign/GlassDesignUI/MainForm.Designer.cs
+++ GlassDesign/GlassDesignUI/MainForm.Designer.cs
@@ -28,13 +28,346 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.D1Label = new System.Windows.Forms.Label();
+            this.D2Label = new System.Windows.Forms.Label();
+            this.D3Label = new System.Windows.Forms.Label();
+            this.D1TextBox = new System.Windows.Forms.TextBox();
+            this.D2TextBox = new System.Windows.Forms.TextBox();
+            this.D3TextBox = new System.Windows.Forms.TextBox();
+            this.H1TextBox = new System.Windows.Forms.TextBox();
+            this.D4TextBox = new System.Windows.Forms.TextBox();
+            this.H2TextBox = new System.Windows.Forms.TextBox();
+            this.D5TextBox = new System.Windows.Forms.TextBox();
+            this.H3TextBox = new System.Windows.Forms.TextBox();
+            this.H1Label = new System.Windows.Forms.Label();
+            this.D4Label = new System.Windows.Forms.Label();
+            this.H2Label = new System.Windows.Forms.Label();
+            this.D5Label = new System.Windows.Forms.Label();
+            this.H3Label = new System.Windows.Forms.Label();
+            this.mmLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.RefreshButton = new System.Windows.Forms.Button();
+            this.BuildButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // D1Label
+            // 
+            this.D1Label.AutoSize = true;
+            this.D1Label.Location = new System.Drawing.Point(12, 13);
+            this.D1Label.Name = "D1Label";
+            this.D1Label.Size = new System.Drawing.Size(139, 17);
+            this.D1Label.TabIndex = 0;
+            this.D1Label.Text = "Диаметр подставки";
+            // 
+            // D2Label
+            // 
+            this.D2Label.AutoSize = true;
+            this.D2Label.Location = new System.Drawing.Point(12, 42);
+            this.D2Label.Name = "D2Label";
+            this.D2Label.Size = new System.Drawing.Size(111, 17);
+            this.D2Label.TabIndex = 1;
+            this.D2Label.Text = "Диаметр ножки";
+            // 
+            // D3Label
+            // 
+            this.D3Label.AutoSize = true;
+            this.D3Label.Location = new System.Drawing.Point(12, 71);
+            this.D3Label.Name = "D3Label";
+            this.D3Label.Size = new System.Drawing.Size(145, 17);
+            this.D3Label.TabIndex = 2;
+            this.D3Label.Text = "Диаметр скругления";
+            // 
+            // D1TextBox
+            // 
+            this.D1TextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.D1TextBox.Location = new System.Drawing.Point(232, 10);
+            this.D1TextBox.Name = "D1TextBox";
+            this.D1TextBox.Size = new System.Drawing.Size(102, 22);
+            this.D1TextBox.TabIndex = 3;
+            // 
+            // D2TextBox
+            // 
+            this.D2TextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.D2TextBox.Location = new System.Drawing.Point(232, 39);
+            this.D2TextBox.Name = "D2TextBox";
+            this.D2TextBox.Size = new System.Drawing.Size(102, 22);
+            this.D2TextBox.TabIndex = 4;
+            // 
+            // D3TextBox
+            // 
+            this.D3TextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.D3TextBox.Location = new System.Drawing.Point(232, 68);
+            this.D3TextBox.Name = "D3TextBox";
+            this.D3TextBox.Size = new System.Drawing.Size(102, 22);
+            this.D3TextBox.TabIndex = 5;
+            // 
+            // H1TextBox
+            // 
+            this.H1TextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.H1TextBox.Location = new System.Drawing.Point(232, 97);
+            this.H1TextBox.Name = "H1TextBox";
+            this.H1TextBox.Size = new System.Drawing.Size(102, 22);
+            this.H1TextBox.TabIndex = 6;
+            // 
+            // D4TextBox
+            // 
+            this.D4TextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.D4TextBox.Location = new System.Drawing.Point(232, 126);
+            this.D4TextBox.Name = "D4TextBox";
+            this.D4TextBox.Size = new System.Drawing.Size(102, 22);
+            this.D4TextBox.TabIndex = 7;
+            // 
+            // H2TextBox
+            // 
+            this.H2TextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.H2TextBox.Location = new System.Drawing.Point(232, 155);
+            this.H2TextBox.Name = "H2TextBox";
+            this.H2TextBox.Size = new System.Drawing.Size(102, 22);
+            this.H2TextBox.TabIndex = 8;
+            // 
+            // D5TextBox
+            // 
+            this.D5TextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.D5TextBox.Location = new System.Drawing.Point(232, 183);
+            this.D5TextBox.Name = "D5TextBox";
+            this.D5TextBox.Size = new System.Drawing.Size(102, 22);
+            this.D5TextBox.TabIndex = 9;
+            // 
+            // H3TextBox
+            // 
+            this.H3TextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.H3TextBox.Location = new System.Drawing.Point(232, 211);
+            this.H3TextBox.Name = "H3TextBox";
+            this.H3TextBox.Size = new System.Drawing.Size(102, 22);
+            this.H3TextBox.TabIndex = 10;
+            // 
+            // H1Label
+            // 
+            this.H1Label.AutoSize = true;
+            this.H1Label.Location = new System.Drawing.Point(12, 100);
+            this.H1Label.Name = "H1Label";
+            this.H1Label.Size = new System.Drawing.Size(101, 17);
+            this.H1Label.TabIndex = 11;
+            this.H1Label.Text = "Высота ножки";
+            // 
+            // D4Label
+            // 
+            this.D4Label.AutoSize = true;
+            this.D4Label.Location = new System.Drawing.Point(12, 129);
+            this.D4Label.Name = "D4Label";
+            this.D4Label.Size = new System.Drawing.Size(118, 17);
+            this.D4Label.TabIndex = 12;
+            this.D4Label.Text = "Диаметр бокала";
+            // 
+            // H2Label
+            // 
+            this.H2Label.AutoSize = true;
+            this.H2Label.Location = new System.Drawing.Point(12, 158);
+            this.H2Label.Name = "H2Label";
+            this.H2Label.Size = new System.Drawing.Size(166, 17);
+            this.H2Label.TabIndex = 13;
+            this.H2Label.Text = "Высота нижнего бокала";
+            // 
+            // D5Label
+            // 
+            this.D5Label.AutoSize = true;
+            this.D5Label.Location = new System.Drawing.Point(12, 186);
+            this.D5Label.Name = "D5Label";
+            this.D5Label.Size = new System.Drawing.Size(136, 17);
+            this.D5Label.TabIndex = 14;
+            this.D5Label.Text = "Диаметр горлышка";
+            // 
+            // H3Label
+            // 
+            this.H3Label.AutoSize = true;
+            this.H3Label.Location = new System.Drawing.Point(12, 214);
+            this.H3Label.Name = "H3Label";
+            this.H3Label.Size = new System.Drawing.Size(170, 17);
+            this.H3Label.TabIndex = 15;
+            this.H3Label.Text = "Высота верхнего бокала";
+            // 
+            // mmLabel
+            // 
+            this.mmLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.mmLabel.AutoSize = true;
+            this.mmLabel.Location = new System.Drawing.Point(340, 13);
+            this.mmLabel.Name = "mmLabel";
+            this.mmLabel.Size = new System.Drawing.Size(26, 17);
+            this.mmLabel.TabIndex = 16;
+            this.mmLabel.Text = "мм";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(340, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 17);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "мм";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(340, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 17);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "мм";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(340, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 17);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "мм";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(340, 129);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 17);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "мм";
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(340, 158);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(26, 17);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "мм";
+            // 
+            // label6
+            // 
+            this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(340, 186);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(26, 17);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "мм";
+            // 
+            // label7
+            // 
+            this.label7.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(340, 214);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(26, 17);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "мм";
+            // 
+            // RefreshButton
+            // 
+            this.RefreshButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.RefreshButton.Location = new System.Drawing.Point(180, 253);
+            this.RefreshButton.Name = "RefreshButton";
+            this.RefreshButton.Size = new System.Drawing.Size(87, 27);
+            this.RefreshButton.TabIndex = 17;
+            this.RefreshButton.Text = "Сбросить";
+            this.RefreshButton.UseVisualStyleBackColor = true;
+            // 
+            // BuildButton
+            // 
+            this.BuildButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.BuildButton.Location = new System.Drawing.Point(273, 253);
+            this.BuildButton.Name = "BuildButton";
+            this.BuildButton.Size = new System.Drawing.Size(97, 27);
+            this.BuildButton.TabIndex = 18;
+            this.BuildButton.Text = "Построить";
+            this.BuildButton.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(382, 298);
+            this.Controls.Add(this.BuildButton);
+            this.Controls.Add(this.RefreshButton);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.mmLabel);
+            this.Controls.Add(this.H3Label);
+            this.Controls.Add(this.D5Label);
+            this.Controls.Add(this.H2Label);
+            this.Controls.Add(this.D4Label);
+            this.Controls.Add(this.H1Label);
+            this.Controls.Add(this.H3TextBox);
+            this.Controls.Add(this.D5TextBox);
+            this.Controls.Add(this.H2TextBox);
+            this.Controls.Add(this.D4TextBox);
+            this.Controls.Add(this.H1TextBox);
+            this.Controls.Add(this.D3TextBox);
+            this.Controls.Add(this.D2TextBox);
+            this.Controls.Add(this.D1TextBox);
+            this.Controls.Add(this.D3Label);
+            this.Controls.Add(this.D2Label);
+            this.Controls.Add(this.D1Label);
+            this.MinimumSize = new System.Drawing.Size(400, 345);
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.Text = "Рюмка";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label D1Label;
+        private System.Windows.Forms.Label D2Label;
+        private System.Windows.Forms.Label D3Label;
+        private System.Windows.Forms.TextBox D1TextBox;
+        private System.Windows.Forms.TextBox D2TextBox;
+        private System.Windows.Forms.TextBox D3TextBox;
+        private System.Windows.Forms.TextBox H1TextBox;
+        private System.Windows.Forms.TextBox D4TextBox;
+        private System.Windows.Forms.TextBox H2TextBox;
+        private System.Windows.Forms.TextBox D5TextBox;
+        private System.Windows.Forms.TextBox H3TextBox;
+        private System.Windows.Forms.Label H1Label;
+        private System.Windows.Forms.Label D4Label;
+        private System.Windows.Forms.Label H2Label;
+        private System.Windows.Forms.Label D5Label;
+        private System.Windows.Forms.Label H3Label;
+        private System.Windows.Forms.Label mmLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button RefreshButton;
+        private System.Windows.Forms.Button BuildButton;
     }
 }
 
